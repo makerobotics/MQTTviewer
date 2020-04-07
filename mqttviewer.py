1b7a38ae928e6c2db4440990cc866b6adf77000f
yann <yann@frankenstrasse.com>
2020-04-07 18:06:37 +0200

    Add humidity html file

--- mqttviewer.py
+++ mqttviewer.py
@@ -3,10 +3,12 @@ import sqlite3
 import json
 
 app = Flask(__name__)
+DB_PATH = "/mnt/nas/mqttdata.db"
+HOST_IP = "192.168.2.201"
 
 @app.route("/data.json")
 def data():
-    connection = sqlite3.connect("/mnt/nas/mqttdata.db")
+    connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
     cursor.execute("select CAST(strftime('%s',timestamp) AS INT)*1000, value from table_data where topic='room/OG/t'")
     results1 = cursor.fetchall()
@@ -17,9 +19,22 @@ def data():
     #print results
     return json.dumps({'results1': results1, 'results2': results2, 'results3': results3})
 
+@app.route("/humidities.json")
+def humidities_data():
+    connection = sqlite3.connect(DB_PATH)
+    cursor = connection.cursor()
+    cursor.execute("select CAST(strftime('%s',timestamp) AS INT)*1000, value from table_data where topic='room/OG/h'")
+    results1 = cursor.fetchall()
+    cursor.execute("select CAST(strftime('%s',timestamp) AS INT)*1000, value from table_data where topic='room/EG/h'")
+    results2 = cursor.fetchall()
+    cursor.execute("select CAST(strftime('%s',timestamp) AS INT)*1000, value from table_data where topic='room/UG/h'")
+    results3 = cursor.fetchall()
+    #print results
+    return json.dumps({'results1': results1, 'results2': results2, 'results3': results3})
+
 @app.route("/temperatures.json")
 def temperatures_data():
-    connection = sqlite3.connect("/mnt/nas/mqttdata.db")
+    connection = sqlite3.connect(DB_PATH)
     cursor = connection.cursor()
     cursor.execute("select CAST(strftime('%s',timestamp) AS INT)*1000, value from table_data where topic='room/OG/t'")
     results1 = cursor.fetchall()
@@ -38,9 +53,13 @@ def graph():
 def temperatures():
     return render_template('temperatures.html')
 
+@app.route("/humidities")
+def humidities():
+    return render_template('humidities.html')
+
 if __name__ == '__main__':
     app.run(
-    debug=True,
-    threaded=True,
-    host='192.168.2.201'
+    debug = True,
+    threaded = True,
+    host = HOST_IP
 )
